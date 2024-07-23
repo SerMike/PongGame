6592aa144f679a71b84a742d946c371aa8fa234f
Michael Brana <SerMike@users.noreply.github.com>
2024-07-23 17:02:16 -0400

    initial scoreboard setup

---
+++ scoreboard.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 280)
