1c259ce05a5ca0035cbc98480b8c4133860da1af
Michael Brana <SerMike@users.noreply.github.com>
2024-07-23 17:25:26 -0400

    added score increase and 'winning' class with 'You Win' message

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,34 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Courier", 20, "normal")
+FONT = ("Courier", 80, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
-        self.goto(0, 280)
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        # Left Player Score
+        self.goto(-100, 200)
+        self.write(self.l_score, align=ALIGNMENT, font=FONT)
+        # Right Player Score
+        self.goto(100, 200)
+        self.write(self.r_score, align=ALIGNMENT, font=FONT)
+
+    def left_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def right_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def winning(self):
+        self.goto(0, 0)
+        self.write("YOU WIN!", align=ALIGNMENT, font=FONT)
