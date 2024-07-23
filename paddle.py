b008359458d11b88c8dd7efac644553d39cf20e8
Michael Brana <SerMike@users.noreply.github.com>
2024-07-23 17:28:08 -0400

    added arrow key controls for right paddle player

--- paddle.py
+++ paddle.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 
-RIGHT_POSITION = (350, 0)
+RIGHT_POSITION = (350, 240)
 LEFT_POSITION = (-350, 0)
 SCREEN_HEIGHT = 600
 MOVE_DISTANCE = 20
@@ -17,6 +17,21 @@ class RightPaddle(Paddle):
     def __init__(self):
         super().__init__()
         self.teleport(RIGHT_POSITION[0], RIGHT_POSITION[1])
+
+    def move_up(self):
+        if self.ycor() < (SCREEN_HEIGHT / 2) - 50:
+            new_y = self.ycor() + MOVE_DISTANCE
+            self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        if self.ycor() > -(SCREEN_HEIGHT / 2) + 50:
+            new_y = self.ycor() - MOVE_DISTANCE
+            self.goto(self.xcor(), new_y)
+
+    def controls(self, screen):
+        screen.listen()
+        screen.onkeypress(self.move_up, "Up")
+        screen.onkeypress(self.move_down, "Down")
 
 class LeftPaddle(Paddle):
     def __init__(self):
